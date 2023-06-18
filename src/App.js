b7411a92bc4eecae26746e8b025760a3c7802a5f
tetianadivnych <113429084+tetianadivnych@users.noreply.github.com>
2023-06-18 20:03:04 +0300

    send image URL to backend and receive response

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import "./styles.css";
 import { JigsawPuzzle } from "react-jigsaw-puzzle/lib";
 import "react-jigsaw-puzzle/lib/jigsaw-puzzle.css";
+import { splitImage } from "./api/services";
 
 const party = require("party-js");
 
@@ -21,6 +22,27 @@ export default function App() {
       });
   }, [solved]);
 
+  //function generatePuzzle
+
+  const generatePuzzle = async () => {
+    try {
+      const puzzlePieces = await splitImage(img); // Call the splitImage function with the image URL
+      console.log(puzzlePieces); // Handle the response from the backend as needed
+
+      // Assuming puzzlePieces is a list of strings
+      if (Array.isArray(puzzlePieces) && puzzlePieces.length > 0) {
+        // Handle the puzzle pieces, e.g., store them in state, display them, etc.
+        // Example: setPieces(puzzlePieces);
+      } else {
+        console.log("Invalid response from the backend"); // Handle the case when the response is not as expected
+      }
+    } catch (error) {
+      console.log(error); // Handle any error that occurred during the API call
+    }
+  };
+
+  //end of generatePuzzle
+
   if (next === 0) {
     return (
       <div className="App">
@@ -37,13 +59,16 @@ export default function App() {
           />
           <button
             style={{ padding: "1rem", color: "#fff", backgroundColor: "#000" }}
-            onClick={() => setPuzzleImg(img)}
+            // onClick={() => setPuzzleImg(img)} //old version to generate puzzle
+            onClick={generatePuzzle} // Call the generatePuzzle function on button click
+
           >
             Generate Puzzle
           </button>
           <button
             style={{ padding: "1rem", color: "#fff", backgroundColor: "#000" }}
             onClick={() => setPuzzleImg(img)}
+
           >
             Verify Puzzle
           </button>
